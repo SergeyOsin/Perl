c31a05870bc7662326c3aa5a7a8a30095a92072f
Sergey Osin <sereosi2017@yandex.ru>
2026-09-14 21:20:42 +0300

    Вторая лр готова

--- lab2/lab2.3.pl
+++ lab2/lab2.3.pl
@@ -2,15 +2,29 @@
 
 print "Введите элементы первого массива: \n";
 @arrayFirst = <STDIN>;
+chomp @arrayFirst;
 
 print "Введите элементы второго массива: \n";
 @arraySecond = <STDIN>;
+chomp @arraySecond;
 
 @arrayResult = ();
 
-for ($i=0;$i<$#arrayFirst + $#arraySecond+2; $i+=2){
-     $arrayResult[$i]=$arrayFirst[$i];
-     $arrayResult[$i+1]=$arraySecond[$i+1];
+$minLen = $#arrayFirst < $#arraySecond ? $#arrayFirst: $#arraySecond;
+
+for ($i=0;$i <= $minLen; $i++){
+     $arrayResult[$i*2]=$arrayFirst[$i];
+     $arrayResult[$i*2+1]=$arraySecond[$i];
 }
 
-print @arrayResult;
+if ($#arrayFirst > $minLen) {
+    for ($j = $minLen + 1; $j <= $#arrayFirst; $j++) {
+        $arrayResult[$j + $minLen + 1] = $arrayFirst[$j];
+    }
+} elsif ($#arraySecond > $minLen) {
+    for ($j = $minLen + 1; $j <= $#arraySecond; $j++) {
+        $arrayResult[$j + $minLen + 1] = $arraySecond[$j];
+    }
+}
+
+print "Итоговый массив: ", "@arrayResult";
